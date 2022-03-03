a48f7f86aba87fc33495964504c36a25668b7144
Valcano Bacon <valcanobacon.github.vmcfd@simplelogin.co>
2022-03-02 23:16:05 -0800

    fixed shortening error

---
+++ tests/test_cli.py
@@ -0,0 +1,8 @@
+from src.cli import SATOSHISTREAM_PUBKEYS, shorten
+
+
+def test_shorten():
+    pubkey = SATOSHISTREAM_PUBKEYS[0]
+    assert shorten(pubkey) == "03c457fa ... 1a8ae1a4"
+    assert shorten(pubkey, segment_length=6) == "03c457 ... 8ae1a4"
+    assert shorten(pubkey, seperator="-") == "03c457fa-1a8ae1a4"
